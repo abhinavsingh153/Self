eeab2181844f4368fb4bab45edf2a27d58364b93
abhinavsingh153 <abhinavsingh153@gmail.com>
2020-03-10 13:56:15 +0530

    Added Journal model and upload journal/thoughts to firebase along with image uploading on firebase storage.

--- app/src/main/java/com/pixelkaveman/self/PostJournalActivity.java
+++ app/src/main/java/com/pixelkaveman/self/PostJournalActivity.java
@@ -7,25 +7,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.util.Date;
+
+import model.Journal;
 import util.JournalApi;
 
 public class PostJournalActivity extends AppCompatActivity  implements View.OnClickListener {
 
     private static final int GALLERY_CODE = 1;
+    private static final String TAG = "postJournalActivity";
     private Button saveButton;
     private ProgressBar progressBar;
     private ImageView addPhotoButton;
@@ -70,6 +82,8 @@ public class PostJournalActivity extends AppCompatActivity  implements View.OnCl
         saveButton = findViewById(R.id.post_save_journal_button);
         imageView = findViewById(R.id.post_imageView);
 
+        progressBar.setVisibility(View.GONE);
+
         imageView = findViewById(R.id.post_imageView);
         saveButton = findViewById(R.id.post_save_journal_button);
         saveButton.setOnClickListener(this);
@@ -104,7 +118,7 @@ public class PostJournalActivity extends AppCompatActivity  implements View.OnCl
         switch (v.getId()) {
             case R.id.post_save_journal_button:
                 //saveJournal
-                //saveJournal();
+                saveJournal();
                 break;
             case R.id.postCameraButton:
                 //get image from gallery/phone
@@ -113,6 +127,98 @@ public class PostJournalActivity extends AppCompatActivity  implements View.OnCl
                 startActivityForResult(galleryIntent, GALLERY_CODE);
                 break;
 
+        }
+    }
+
+    private void saveJournal(){
+        //TODO: save the journal and navigate ot the journal list screen
+        final String title = titleEditText.getText().toString().trim();
+        final String thoughts = thoughtsEditText.getText().toString().trim();
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        if(!TextUtils.isEmpty(title)
+        && !TextUtils.isEmpty(thoughts)
+        && imageUri != null){
+
+            final StorageReference filePath = storageReference //.../journal_images/our_image.jpeg
+                    .child("journal_images")
+                    .child("my_image" + Timestamp.now().getSeconds()); // my_image_887474737
+
+            //upload image on firebase storage
+            filePath.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+
+
+                            filePath.getDownloadUrl()
+                                    .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+
+                                            String imageUrl = uri.toString();
+
+                                            //TODO: Create a journal object - model
+                                            Journal journal = new Journal();
+                                            journal.setTitle(title);
+                                            journal.setThought(thoughts);
+                                            journal.setImageUrl(imageUrl);
+                                            journal.setTimeAdded(new Timestamp(new Date()));
+                                            journal.setUserName(currentUserName);
+                                            journal.setUserId(currentUserId);
+
+                                            //TODO: invoke our collectionReference
+                                            collectionReference.add(journal)
+                                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                        @Override
+                                                        public void onSuccess(DocumentReference documentReference) {
+
+                                                            progressBar.setVisibility(View.INVISIBLE);
+
+                                                            startActivity(new Intent(PostJournalActivity.this ,
+                                                                    JournalListActivity.class));
+                                                            finish();
+                                                        }
+                                                    })
+                                                    .addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Toast.makeText(PostJournalActivity.this ,
+                                                                    "Sorry, your thought was not uploaded" ,
+                                                                    Toast.LENGTH_LONG).show();
+                                                            Log.d(TAG , "onFailure: " + e.getMessage());
+
+
+                                                        }
+                                                    });
+
+                                            //TODO: and save a journalinstance
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.d(TAG , "onFailure: " + e.getMessage());
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Log.d(TAG , "onFailure: " + e.getMessage());
+                        }
+                    });
+
+        }
+        else{
+
+            Toast.makeText(PostJournalActivity.this , "Please enter the required fields before saving" , Toast.LENGTH_LONG).show();
+            Log.d(TAG ,"Please enter the required fields before saving" );
         }
     }
 
