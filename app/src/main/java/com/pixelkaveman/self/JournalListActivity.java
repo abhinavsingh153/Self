c6faa28a2c0bb69e041225c415fcdf7ce02b186f
abhinavsingh153 <abhinavsingh153@gmail.com>
2020-03-10 19:40:33 +0530

    Implemented menu items /signout/add journals

--- app/src/main/java/com/pixelkaveman/self/JournalListActivity.java
+++ app/src/main/java/com/pixelkaveman/self/JournalListActivity.java
@@ -1,14 +1,75 @@
 package com.pixelkaveman.self;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class JournalListActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private com.google.firebase.auth.FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_list);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.action_add:
+                //Take users to add journal
+                if(user != null && firebaseAuth != null){
+
+                    startActivity(new Intent(JournalListActivity.this , PostJournalActivity.class));
+                    //finish();
+                }
+                break;
+
+            case R.id.action_signout:
+                //sign user out
+                if (user != null && firebaseAuth != null){
+                    firebaseAuth.signOut();
+
+                    startActivity(new Intent(JournalListActivity.this ,
+                            MainActivity.class));
+                    //finish();
+                }
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //here we will get all our journals
     }
 }
