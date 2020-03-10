eae1fa0a9602b29f386f0a9ad9ec52d50bc095d4
abhinavsingh153 <abhinavsingh153@gmail.com>
2020-03-10 12:44:00 +0530

    Added UI for postJournal activity

--- app/src/main/java/com/pixelkaveman/self/CreateAccountActivity.java
+++ app/src/main/java/com/pixelkaveman/self/CreateAccountActivity.java
@@ -3,31 +3,45 @@ package com.pixelkaveman.self;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import util.JournalApi;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private static final String TAG =  "createAccountActivity";
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseUser currentUser;
 
+    //Firestore
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private CollectionReference collectionReference = db.collection("Users");
 
     private Button createAccountButton;
 
@@ -48,6 +62,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.create_acct_progress);
         createAccountButton = findViewById(R.id.create_account_button);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -68,10 +83,22 @@ public class CreateAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String email = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String username = usernameEditText.getText().toString();
-                createUserEmailAccount(email , password , username);
+                if (!TextUtils.isEmpty(usernameEditText.getText().toString())
+                && !TextUtils.isEmpty(emailEditText.getText().toString())
+                && !TextUtils.isEmpty(passwordEditText.getText().toString())) {
+
+                    String username = usernameEditText.getText().toString().trim();
+                    String email = emailEditText.getText().toString().trim();
+                    String password = passwordEditText.getText().toString().trim();
+
+                    createUserEmailAccount(email, password, username);
+                }
+                else{
+
+                    Toast.makeText(CreateAccountActivity.this ,
+                            "Empty fields not allowed." ,
+                            Toast.LENGTH_LONG);
+                }
             }
         });
     }
@@ -96,7 +123,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                                 //we take user to the AddJournalActivity.
 
                                 currentUser = firebaseAuth.getCurrentUser();
-                                String currentUserId = currentUser.getUid();
+                                final String currentUserId = currentUser.getUid();
 
                                 //create a map so we can create a user in the user ciollection in the firestore.
                                 Map<String , String> userObj = new HashMap<>();
@@ -105,15 +132,64 @@ public class CreateAccountActivity extends AppCompatActivity {
 
                                 //save to our firestore database.
 
+                                collectionReference.add(userObj)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+
+                                                documentReference.get()
+                                                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                                                                if (Objects.requireNonNull(task.getResult().exists())){
+
+                                                                    progressBar.setVisibility(View.INVISIBLE);
+
+                                                                    String name = task.getResult()
+                                                                            .getString("username");
+
+                                                                    JournalApi journalApi = JournalApi.getInstance(); // Global API
+                                                                    journalApi.setUsername(name);
+                                                                    journalApi.setUserId(currentUserId);
+
+                                                                    Intent intent = new Intent(CreateAccountActivity.this ,
+                                                                            PostJournalActivity.class);
+                                                                    intent.putExtra("username" , name);
+                                                                    intent.putExtra("userId" , currentUserId);
+                                                                    startActivity(intent);
+
+                                                                }
+                                                                else{
+
+                                                                    progressBar.setVisibility(View.INVISIBLE);
+
+                                                                }
+                                                            }
+                                                        });
+
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+
+                                            }
+                                        });
+
                             }else{
 
                                 //something went wrong
+                                Toast.makeText(CreateAccountActivity.this , "Something went wrong" , Toast.LENGTH_LONG).show();
+                                Log.d(TAG , task.toString());
                             }
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
+
+                            Log.d(TAG , e.toString());
 
                         }
                     });
--- app/src/main/java/com/pixelkaveman/self/PostJournalActivity.java
+++ app/src/main/java/com/pixelkaveman/self/PostJournalActivity.java
@@ -1,14 +1,152 @@
 package com.pixelkaveman.self;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class PostJournalActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import util.JournalApi;
+
+public class PostJournalActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private static final int GALLERY_CODE = 1;
+    private Button saveButton;
+    private ProgressBar progressBar;
+    private ImageView addPhotoButton;
+    private EditText titleEditText;
+    private EditText thoughtsEditText;
+    private TextView currentUserTextView;
+    private ImageView imageView;
+
+    private String currentUserId;
+    private String currentUserName;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+
+    //Connection to Firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private StorageReference storageReference;
+
+    private CollectionReference collectionReference = db.collection("Journal");
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_journal);
+
+//        Bundle bundle = getIntent().getExtras();
+//
+//        if(bundle != null){
+//            String username = bundle.getString("username");
+//            String userId = bundle.getString("userId");
+//        }
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.post_progressBar);
+        titleEditText = findViewById(R.id.post_title_et);
+        thoughtsEditText = findViewById(R.id.post_description_et);
+        currentUserTextView = findViewById(R.id.post_username_textview);
+        saveButton = findViewById(R.id.post_save_journal_button);
+        imageView = findViewById(R.id.post_imageView);
+
+        imageView = findViewById(R.id.post_imageView);
+        saveButton = findViewById(R.id.post_save_journal_button);
+        saveButton.setOnClickListener(this);
+        addPhotoButton = findViewById(R.id.postCameraButton);
+        addPhotoButton.setOnClickListener(this);
+
+        if(JournalApi.getInstance() != null){
+            currentUserId = JournalApi.getInstance().getUserId();
+            currentUserName = JournalApi.getInstance().getUsername();
+
+            currentUserTextView.setText(currentUserName );
+        }
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                user = firebaseAuth.getCurrentUser();
+
+                if (user != null){
+
+                }
+                else{
+
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.post_save_journal_button:
+                //saveJournal
+                //saveJournal();
+                break;
+            case R.id.postCameraButton:
+                //get image from gallery/phone
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_CODE);
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK){
+            
+            if (data != null){
+                
+                // get the path of the image picked and store int he image uri
+                imageUri = data.getData();
+                imageView.setImageURI(imageUri);
+
+
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        user = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuth != null){
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
